2f3689687b7ee17242922105bc735fdcf3749d92
MooradAltamimi <moorad2003@gmail.com>
2019-07-04 21:08:34 +0100

    Pieces are selectable 😁 but not functional yet 😐

--- index.js
+++ index.js
@@ -7,21 +7,31 @@ var pieces = [
 	[2, 2, 2, 2],
 	[2, 2, 2, 2],
 	[2, 2, 2, 2]
-]
+];
+var selectedPiece = [-1, -1];
 
 function generateBoard() {
 	let board = document.getElementById('game');
+	let count = 0;
 	for (var i = 0; i < 8; i++) {
 		for (var j = 0; j < 8; j++) {
+
 			let slot = document.createElement('div');
 
 			if (j % 2 == i % 2) {
+
 				slot.appendChild(document.createElement('img'));
 				slot.className += ' playable';
+				slot.setAttribute('index', count);
+				slot.onclick = function () {
+					select(this);
+				}
+				count++;
 			} else {
 				slot.className += ' none';
 			}
 			board.appendChild(slot);
+
 		}
 	}
 }
@@ -32,13 +42,25 @@ function drawPieces() {
 		for (var j = 0; j < 4; j++) {
 
 			if (pieces[i][j] == 1) {
-				console.log(board[i + j].firstChild)
 				board[(i * 4) + j].firstChild.src = 'Sprites/black.png';
 			} else if (pieces[i][j] == 2) {
 				board[(i * 4) + j].firstChild.src = 'Sprites/red.png';
 			}
 		}
 	}
+}
+
+function select(element) {
+	let index = element.getAttribute('index');
+
+	if (pieces[Math.floor(index / 4)][index % 4] == 1 || pieces[Math.floor(index / 4)][index % 4] == 2) {
+		for (var i of document.getElementsByClassName('selected')) {
+			i.classList.remove('selected');
+		}
+		element.className += ' selected';
+		selectedPiece = [Math.floor(index / 4), index % 4]
+	}
+	console.log(selectedPiece)
 }
 
 function main() {
